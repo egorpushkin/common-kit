eba6509fbaf80d7f7c5eea96e9fc298dbc247283
Egor Pushkin <egor.pushkin@gmail.com>
2009-07-02 14:22:41 +0000

    IThread::Sleep was implemented for *nix platforms.
    git-svn-id: https://commonkit.googlecode.com/svn/trunk@90 2aaa8e9b-9c4f-0410-8024-4f79e9a52ea3

--- source/source/MinCOMLib/Concurrency/Core/IThread.posix.cpp
+++ source/source/MinCOMLib/Concurrency/Core/IThread.posix.cpp
@@ -2,6 +2,11 @@
 
 namespace MinCOM
 {
+
+	void IThread::Sleep(unsigned long delay)
+	{
+		usleep(delay * 1000); // Converting delay from milliseconds to microseconds.
+	}
     
     int IThread::GetSelfId()
     {
